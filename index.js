a48ea889ece5b46a1a24016e9b5b2faf50febd44
Khalid Altuma <82910367+khalid-altuma@users.noreply.github.com>
2022-08-14 19:05:57 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,91 @@
+var userClickedPattren = [];
+
+var gamePattren = [];
+
+var buttonColor = ["red", "blue", "yellow", "green"];
+
+var started = false;
+var level = 0;
+
+$(document).keydown(function () {
+    if (!started) {
+        $("#level-title").text("level " + level);
+        nextSequence();
+        started = true;
+    }
+});
+
+function checkAnswer(currentLevel) {
+    
+    if (gamePattren[currentLevel] === userClickedPattren[currentLevel]) {
+        console.log("success");
+        if (gamePattren.length === userClickedPattren.length) {
+            setTimeout(function () {
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        console.log("wrong");
+
+        playSound("wrong");
+
+        $("body").addClass("game-over");
+        setTimeout( function() {
+        $("body").removeClass("game-over");
+        }, 200);
+        
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+
+        startOver();
+    }
+}
+
+function nextSequence() {
+    userClickedPattren = [];
+
+    level++;
+    $("#level-title").text("level " + level);
+
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColor = buttonColor[randomNumber];
+    gamePattren.push(randomChosenColor);
+
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColor);
+
+}
+
+
+$(".btn").click( function () {
+
+    var userChosenColor = $(this).attr("id");
+    userClickedPattren.push(userChosenColor);
+
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+
+    checkAnswer(userClickedPattren.length-1);
+
+})
+
+function startOver() {
+    started = false;
+    level = 0;
+    gamePattren = [];
+    
+}
+
+function playSound(name) {
+    var audio = new Audio('sounds/' + name + '.mp3');
+    audio.play();
+}
+
+function animatePress(name) {
+    $("#" + name ).addClass("pressed");
+    setTimeout(function () {
+        $("#" + name ).removeClass("pressed");
+    }, 100);
+}
+
+
+
